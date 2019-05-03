8a9cf137f1e04375389c311ffdfe7805d07c9164
Bratela <Bratela@users.noreply.github.com>
2019-05-03 09:08:58 +0300

    Update lesson15.rb

--- lesson15.rb
+++ lesson15.rb
@@ -1,5 +1,5 @@
 my_data = {:name => "James", :famaly_name => "Bond"}
-my_data2 = {name:"James", famaly_name: "Bond"}
+my_data2 = {name: "James", famaly_name: "Bond"}
 
 puts my_data[:name]
 puts my_data[:famaly_name]
